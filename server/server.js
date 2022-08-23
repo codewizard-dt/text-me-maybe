d8de8b1f22d4c7a33cada75804c066670fb33329
Andrew Edwards <106359255+Andrew87E@users.noreply.github.com>
2022-08-23 18:58:54 -0500

    tweaks that i think made it work

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+const publicPath = path.join(process.cwd(), "client", dist);
 
-app.use(express.static('../client/dist'));
+app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
-app.listen(PORT, () => console.log(`Now listening on port: http://localhost:${PORT} `));
+app.listen(PORT, () =>
+  console.log(`Now listening on port: http://localhost:${PORT} `)
+);
