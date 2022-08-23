71e8f4d316c414d540636d706d9926b5b349535b
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-08-23 15:56:52 -0500

    package.json update

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = (app) =>
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-  );
+  app.get('/', (req, res) => {
+    let indexPath = path.resolve(__dirname, '../client/dist/index.html')
+    console.log(indexPath)
+    res.sendFile(indexPath)
+  });
