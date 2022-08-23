2a1de34b81d7fab9004d852fa900e42120b7de9d
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-08-23 17:03:11 -0500

    tweak Heroku

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) => {
-    let indexPath = path.resolve(__dirname, '../client/dist/index.html')
+    let indexPath = path.join(__dirname, '../client/dist/index.html')
     console.log("HEYYYY", indexPath)
     return res.sendFile(indexPath)
   });
