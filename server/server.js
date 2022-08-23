2891a41adf9fba4ca04d7ba33b95938ed7b0cb60
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-08-23 15:10:04 -0500

    adjusting pathnames for heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../dist'));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
