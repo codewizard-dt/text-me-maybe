b162d14d5b78905b3574e38423de7aef6725fa44
David Taylor <94571813+codewizard-dt@users.noreply.github.com>
2022-08-23 20:13:26 -0500

    tweak server

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../../client/dist/index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/dist/index.html'))
     // res.send("hello")
   );
